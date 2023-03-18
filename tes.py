940bef98099bda0c6547f71337a53bdadbec788d
D3M09 <91779473+D3M09@users.noreply.github.com>
2023-03-18 15:09:03 +0600

    Update tes.py
    Signed-off-by: D3M09 <91779473+D3M09@users.noreply.github.com>

--- tes.py
+++ tes.py
@@ -1,2 +1,13 @@
-from tes import login
-login()
+import os,time,platform
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('Device 64 Bit')
+    from tes import login
+    login()
+
+if bit == '32bit':
+    print('Device 32 Bit')
+    from tes32 import login
+    login()
+else:
+    print('Device LOL')
